a8358cd6ca0222c00ec771c06e8840d2a7571afb
Drew Myers <drew@drewmyers.dev>
2025-12-21 17:28:32 -0500

    initial x230 commit

---
+++ thinkpad-x230/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        nixos-hardware.nixosModules.lenovo-thinkpad-x230
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.acmyers = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
---
+++ thinkpad-x230/home.nix
@@ -0,0 +1,148 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "acmyers";
+  home.homeDirectory = "/home/acmyers";
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    neofetch
+    nnn # terminal file manager
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils  # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    glow # markdown previewer in terminal
+
+    btop  # replacement of htop/nmon
+  ];
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Drew Myers";
+        email = "drew@drewmyers.dev";
+      };
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 10;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs = {
+    nushell = {
+        enable = true;
+
+        # The config.nu can be anywhere you want if you like to edit your Nushell with Nu
+        # configFile.source = /home/acmyers/.config/nushell/config.nu;
+        # for editing directly to config.nu 
+         
+        extraConfig = ''
+         let carapace_completer = {|spans|
+         carapace $spans.0 nushell ...$spans | from json
+         }
+         $env.config = {
+          show_banner: false,
+          completions: {
+          case_sensitive: false # case-sensitive completions
+          quick: true    # set to false to prevent auto-selecting completions
+          partial: true    # set to false to prevent partial filling of the prompt
+          algorithm: "fuzzy"    # prefix or fuzzy
+          external: {
+          # set to false to prevent nushell looking into $env.PATH to find more suggestions
+              enable: true 
+          # set to lower can improve completion performance at the cost of omitting some options
+              max_results: 100 
+              completer: $carapace_completer # check 'carapace_completer' 
+            }
+          }
+         } 
+         $env.PATH = ($env.PATH | 
+         split row (char esep) |
+         prepend /home/myuser/.apps |
+         append /usr/bin/env
+         )
+         '';
+
+       shellAliases = {
+         vi = "hx";
+         vim = "hx";
+         nano = "hx";
+       };
+     };  
+     carapace.enable = true;
+     carapace.enableNushellIntegration = true;
+
+     starship = { enable = true;
+         settings = {
+           add_newline = true;
+           character = { 
+           success_symbol = "[➜](bold green)";
+           error_symbol = "[➜](bold red)";
+         };
+      };
+    };
+  };
+
+  home.stateVersion = "25.11";
+}
