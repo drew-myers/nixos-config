6448382c13511f2e23791e5f2dc25af0909b8d46
Drew Myers <drew@drewmyers.dev>
2025-12-26 20:54:59 -0500

    add trigkey box

---
+++ trigkey/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.trigkey = nixpkgs.lib.nixosSystem {
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.acmyers = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
